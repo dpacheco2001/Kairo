73f910f59c6e423d2e1314ffbff6100b48777cf2
Diego Pacheco <d.pacheco@pucp.edu.pe>
2025-04-08 01:57:00 -0500

    feat: Update dependencies and configure API Gateway with Eureka integration

--- api-gateaway/src/test/java/com/kairo/api_gateway/ApiGateawayApplicationTests.java
+++ api-gateaway/src/test/java/com/kairo/api_gateway/ApiGateawayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kairo.api_gateaway;
+package com.kairo.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGateawayApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
